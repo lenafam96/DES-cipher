536326de3d5df30008e60fb4945fe31dd553ce8d
Bun-Boo <ios02052001@gmail.com>
2022-10-10 10:58:56 +0700

    update base encrypt

--- ascii2bin.py
+++ ascii2bin.py
@@ -2,12 +2,13 @@ import math
 
 
 def toBinary(a):
-  l,m=[],[]
-  for i in a:
-    l.append(ord(i))
-  for i in l:
-    m.append(int(bin(i)[2:]))
-  return m
+    l, m = [], []
+    for i in a:
+        l.append(ord(i))
+    for i in l:
+        m.append(int(bin(i)[2:]))
+    return m
 
-print("PHANVANH") 
-print(toBinary("PHANVANH"))
+
+print("PHANVANH")
+print(toBinary("ê"))
